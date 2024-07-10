a2272f4a1c74a168a69dceaf9db6f8df73fb071d
Joey Scanga <scangjo@gmail.com>
2024-07-10 13:51:47 -0500

    method of getting kwargs is more robust. the value for each option can now be more than 1 token long

--- oceanproc/web/get_parser_json.py
+++ oceanproc/web/get_parser_json.py
@@ -11,6 +11,23 @@ import sys
 from textwrap import dedent
 
 def _create_argument_dicts(path: str, parser_group_names: list[dict]):
+    def get_opt(node):
+        toklist = list(script_ast.get_tokens(node))
+        optname = [re.sub(r'\"--(.*)\"', r'\1', t.string) for t in toklist if 
+                   t.type == 3 and 
+                   ('--' in t.string or '-' not in t.string)][0]
+        equalindices = [idx for idx in range(len(toklist)) if toklist[idx].string == "="]
+        optsettingkeys = [toklist[idx-1].string for idx in equalindices]
+        optsettingvals = []
+        for startidx in equalindices:
+            optsettingval = ""
+            idx = startidx + 1
+            while toklist[idx].string != "," and idx < len(toklist) - 1:
+                optsettingval += re.sub(r'^[\"\'](.*)[\"\']$', r'\1', toklist[idx].string)
+                idx += 1
+            optsettingvals.append(optsettingval)
+        option = {k: v for (k, v) in zip(optsettingkeys, optsettingvals)}
+        return option
     arguments = []
     with open(path) as f:
         script_ast = asttokens.ASTTokens(f.read(), parse=True)
@@ -21,14 +38,7 @@ def _create_argument_dicts(path: str, parser_group_names: list[dict]):
                 node.func.value.id in parser_group_names and
                 isinstance(node.func.attr, str) and
                 node.func.attr == 'add_argument'):
-                    toklist = list(script_ast.get_tokens(node))
-                    # Grab the option name, should work for both positional and keyword args
-                    optname = [re.sub(r'\"--(.*)\"', r'\1', t.string) for t in toklist if 
-                               t.type == 3 and 
-                               ('--' in t.string or '-' not in t.string)][0]
-                    optsettings = {toklist[idx-1].string: re.sub(r'^[\'\"](.*)[\'\"]$', r'\1', toklist[idx+1].string) for idx in range(len(toklist))
-                                   if toklist[idx].string == "="}  
-                    arguments.append({optname: optsettings})
+                    arguments.append(get_opt(node))
     return arguments
 
 def generate_json_from_parser_options(config_path: str = 'config.json') -> list[dict]:
