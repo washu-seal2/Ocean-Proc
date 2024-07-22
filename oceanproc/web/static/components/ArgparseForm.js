fe5abfa42245a73a58dc0fd2f014b290e5afd70b
Joey Scanga <scangjo@gmail.com>
2024-07-22 11:23:34 -0500

    added ArgparseForm, ArgparseFormEntry components

---
+++ oceanproc/web/static/components/ArgparseForm.js
@@ -0,0 +1,9 @@
+import { html } from 'htm/preact';
+import { render } from 'preact';
+import { useState, useEffect, useRef, useMemo } from 'preact/hooks';
+import { FileBrowser } from './FileBrowser.js';
+
+export function ArgparseForm(props) {
+	function sortByInputType(a, b){
+	}
+}
