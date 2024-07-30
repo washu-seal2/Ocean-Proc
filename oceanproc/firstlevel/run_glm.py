e109898cd4c39f85366e418e2de6eed78b7d1c03
Ramone Agard <rhagard@wustl.edu>
2024-07-30 11:53:58 -0500

    indent json arg file

--- oceanproc/firstlevel/run_glm.py
+++ oceanproc/firstlevel/run_glm.py
@@ -452,7 +452,7 @@ def main():
                     opts_to_save[a.option_strings[0]] = all_opts[a.dest]
         with open(args.export_args, "w") as f:
             if args.export_args.endswith(".json"):
-                f.write(json.dumps(opts_to_save))
+                f.write(json.dumps(opts_to_save, indent=4))
             else:
                 for k,v in opts_to_save.items():
                     f.write(f"{k}{make_option(value=v)}\n")
